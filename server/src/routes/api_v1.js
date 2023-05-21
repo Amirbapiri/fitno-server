2959786a0584b8b2b2b5b7add3396cbc7353390b
Amir <amircodes10@gmail.com>
2023-05-22 00:33:22 +0330

    add new routes

--- server/src/routes/api_v1.js
+++ server/src/routes/api_v1.js
@@ -5,6 +5,7 @@ const clientsRouter = require("./clients/clients.routers");
 const plansRouter = require("./plans/plans.router");
 const clientRequestsRouter = require("./client_requests/client_requests.router");
 const ticketsRouter = require("./tickets/tickets.routers");
+const exercisesRouter = require("./exercises/exercises_routes");
 
 const api = express.Router();
 
@@ -13,6 +14,8 @@ api.use("/clients", clientsRouter);
 api.use("/requests", clientRequestsRouter);
 api.use("/plans", plansRouter);
 api.use("/tickets", ticketsRouter);
+api.use("/exercises", exercisesRouter);
+
 
 module.exports = {
   api_v1: api,
