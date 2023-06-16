e02586b8a6493a0725e26cbfa13402fd138edfca
Amir <amircodes10@gmail.com>
2023-06-17 00:16:17 +0330

    Integrate Swagger for API documentation

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./../../swagger.json");
 
 const { api_v1 } = require("./routes/api_v1")
 
@@ -8,6 +10,8 @@ app.use(cors({
   origin: "http://localhost:3000",
 }));
 app.use(express.json());
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/v1", api_v1);
 
